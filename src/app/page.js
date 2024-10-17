927ff9132c9abd22c4a2ecee5945432c1f032463
Karol Saganowski <126456750+Ninjer2137@users.noreply.github.com>
2024-10-17 23:39:48 +0200

    działają strzałki

--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,47 @@
 "use client"
 import { Card, CardContent } from "@/components/ui/card";
-import { ArrowDown, ArrowUp } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ArrowUp, ArrowDown } from 'lucide-react';
 
 export default function Home() {
-  const [data, setData] = useState([""])
-  const [err, setErr] = useState(false)
-  const [load, setLoad] = useState(true)
+  const [dane, setDane] = useState([""])
 
   useEffect(()=>{
     const getData = async() =>{
       try{
         const dataR = await fetch("https://api.nbp.pl/api/cenyzlota/last/30/?format=json")
         const dataJson = await dataR.json()
-        setData(dataJson.reverse())
-      }catch(err){
-        setErr(true)
-        console.err("błąd")
+        setDane(dataJson.reverse())
+      }catch(error){
+
       }finally{
-        setLoad(false)
+        
       }
     }
     getData()
   },[])
 
-
-
-  const dat = data && data.map((item, idx) => 
-  {
-  if(item[idx+1].cena>item[idx].cena){
-    const arrow = <ArrowUp size={60} color="green"/>
-  }else{
-    const arrow = <ArrowDown size={60} color="red"/>
-  }
-  }
-  )
-
-//  if(prevPrice!=null){
-//    const arrow = curPrice > prevPrice ? <ArrowUp size={60} color="green"/> : <ArrowDown size={60} color="red"/>
-//  }
-
-  return (
-    <>
-    <div className="flex flex-row flex-wrap justify-center gap-5">
-    <h1>{err && "błąd"}</h1>
-    <h1>{load && "ładowanie"}</h1>
-    {
-      data && data.map((item, idx) => 
-        <Card className="border p-6" key={idx}>
-          <CardContent>{item.data}</CardContent>    
-          <CardContent>{item.cena}</CardContent>
-          {arrow}
+return (
+  <div className="flex flex-row flex-wrap justify-center m-10">
+    {dane && dane.map((dane, idx, prev) => (
+      idx > 0 && dane.cena > prev[idx - 1].cena ? (
+        <Card className="h-28 w-64 m-1 pt-4" key={idx}>
+          <CardContent className>
+            <ArrowUp className="text-green-500 float-right size-16"/>
+            <h1 className="text-3xl">{dane.cena}</h1>
+            <h1 className="text-xl">{dane.data}</h1>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="h-28 w-64 m-1 pt-4" key={idx}>
+          <CardContent>
+            <ArrowDown className="text-red-500 float-right size-16"/>
+            <h1 className="text-3xl">{dane.cena}</h1>
+            <h1 className="text-xl">{dane.data}</h1>
+          </CardContent>
         </Card>
       )
-    }
-    </div>  
-  </>
-  );
+    ))}
+  </div>
+);
 }
