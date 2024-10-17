a84c1a6d15316ee597e8f1625108873702b7f032
Karol Saganowski <126456750+Ninjer2137@users.noreply.github.com>
2024-10-17 12:15:09 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,60 @@
-import { ModeToggle } from "@/components/themeSwitcher";
-import Image from "next/image";
+"use client"
+import { Card, CardContent } from "@/components/ui/card";
+import { ArrowDown, ArrowUp } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = useState([""])
+  const [err, setErr] = useState(false)
+  const [load, setLoad] = useState(true)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <ModeToggle></ModeToggle>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(()=>{
+    const getData = async() =>{
+      try{
+        const dataR = await fetch("https://api.nbp.pl/api/cenyzlota/last/30/?format=json")
+        const dataJson = await dataR.json()
+        setData(dataJson.reverse())
+      }catch(err){
+        setErr(true)
+        console.err("błąd")
+      }finally{
+        setLoad(false)
+      }
+    }
+    getData()
+  },[])
+
+
+
+  const dat = data && data.map((item, idx) => 
+  {
+  if(item[idx+1].cena>item[idx].cena){
+    const arrow = <ArrowUp size={60} color="green"/>
+  }else{
+    const arrow = <ArrowDown size={60} color="red"/>
+  }
+  }
+  )
+
+//  if(prevPrice!=null){
+//    const arrow = curPrice > prevPrice ? <ArrowUp size={60} color="green"/> : <ArrowDown size={60} color="red"/>
+//  }
+
+  return (
+    <>
+    <div className="flex flex-row flex-wrap justify-center gap-5">
+    <h1>{err && "błąd"}</h1>
+    <h1>{load && "ładowanie"}</h1>
+    {
+      data && data.map((item, idx) => 
+        <Card className="border p-6" key={idx}>
+          <CardContent>{item.data}</CardContent>    
+          <CardContent>{item.cena}</CardContent>
+          {arrow}
+        </Card>
+      )
+    }
+    </div>  
+  </>
   );
 }
